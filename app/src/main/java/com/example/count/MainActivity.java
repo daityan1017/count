1b7a9ca0e72dbb4774865c6b591b29c1c4b9b5b2
DAITYAN1017 <nettouroku.daityan@gmail.com>
2019-08-26 23:36:28 +0900

    p

--- app/src/main/java/com/example/count/MainActivity.java
+++ app/src/main/java/com/example/count/MainActivity.java
@@ -4,23 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private TextView textView;
     private int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button)
-                .setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        ((TextView) findViewById(R.id.textView)).setText(String.format("%d 回目",++i));
-                        //401ページまで
-                    }
-                });
+
+        //ビューグループのインスタンスを生成
+        LinearLayout linearLayout = new LinearLayout(getApplication());
+        //表示サイズをアクティビティ全体に設定
+        linearLayout.setLayoutParams(
+                new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                ));
+        //縦並びのレイアウトを設定
+        linearLayout.setOrientation(linearLayout.VERTICAL);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        textView.setText(String.format("%d 回目",++i));
+        //403ページまで
     }
 }
